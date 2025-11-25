be1d136918ee13a71a36a8dfe79a6c23222479cb
abduqodir <ttheonepersonn@gmail.com>
2025-11-25 15:11:22 +0000

    all models login,logout

--- hoots/settings.py
+++ hoots/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from django.urls import reverse_lazy
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,6 +123,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
+
+
+
+LOGIN_REDIRECT_URL = reverse_lazy('home')  # 'home' is your URL name
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView,LogoutView 
 from django.views.generic import CreateView ,ListView,UpdateView,DeleteView
 from django.contrib.auth.models import User
@@ -8,9 +9,12 @@ class UserRegister(CreateView):
     model=User
     form_class=cuserform
     template_name='register.html'
+    success_url=reverse_lazy('login')
 
 class Userlogin(LoginView):
     template_name='login.html'
+    success_url = reverse_lazy('home')
+
 
 
 
