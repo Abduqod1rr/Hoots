2eba454839329a6d4f530d37c42022f98e13b9a7
abduqodir <ttheonepersonn@gmail.com>
2025-11-25 14:33:48 +0000

    model Hoots

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Hoots(models.Model):
+    owner=models.ForeignKey(User,on_delete=models.CASCADE)
+    title=models.CharField(max_length=20,default='no title')
+    hoot=models.FileField(upload_to='posts', null=True)
+    posted_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("register/", views.UserRegister.as_view(), name="register"),
-    path("login/", views.Userlogin.as_view(), name="login")
+    path("login/", views.Userlogin.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout")
 ]
